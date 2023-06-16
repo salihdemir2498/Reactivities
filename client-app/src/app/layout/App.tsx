e419cb2fa3b3c4ec4a3d10e6f2c1ec598b0ece57
Salih Demir <salihhdemir1727@gmail.com>
2023-06-16 15:38:38 +0300

    end of section 5

---
+++ client-app/src/app/layout/App.tsx
@@ -0,0 +1,74 @@
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+import { Container} from 'semantic-ui-react';
+import { Activity } from '../models/activity';
+import NavBar from './NavBar';
+import ActivityDashboard from '../../features/activities/dashboard/ActivityDashboard';
+import {v4 as uuid} from 'uuid';
+
+function App() {
+  const [activities, setActivities] = useState<Activity[]>([]);
+  const [selectedActivity, setSelectedActivity] = useState<Activity | undefined>(undefined);
+  const [editMode, setEditMode] = useState(false);
+
+
+  useEffect(() => {
+    axios.get<Activity[]>("http://localhost:5000/api/activities")
+      .then(response => {
+        setActivities(response.data)
+      })
+  }, [])
+
+
+  function handleSelectActivity(id:string) {
+    setSelectedActivity(activities.find(x => x.id === id));
+  }
+
+  function handleCancelSelectActivity() {
+    setSelectedActivity(undefined);
+  }
+
+  function handleFormOpen(id?: string){
+    id ? handleSelectActivity(id) : handleCancelSelectActivity();
+    setEditMode(true);
+  }
+
+  function handleFormClose(){
+    setEditMode(false);
+  }
+
+  function handleCreateOrEditActivity(activity: Activity){
+    activity.id 
+      ? setActivities([...activities.filter(x => x.id !== activity.id), activity]) //activities dizisinden activity.id'ye eşit olmayan diğer etkinlikleri filtreler ve ardından activity'yi bu filtreden geçen etkinliklerin sonuna ekler.
+      : setActivities([...activities, {...activity, id: uuid()}]);//activity.id değeri yoksa, yani etkinlik yeni bir etkinlikse, setActivities işlevi kullanılarak activities dizisine activity'yi ekler.
+      setEditMode(false);
+      setSelectedActivity(activity);
+  }
+
+  function handleDeleteActivity(id: string){
+    setActivities([...activities.filter(x => x.id !== id)]) //activities dizisinden activity.id'ye eşit olmayan diğer etkinlikleri filtreler ve ardından activity'yi bu filtreden geçen etkinliklerin sonuna ekler.  
+    setSelectedActivity(undefined);
+  }
+
+  return (
+    <>
+      <NavBar openForm={handleFormOpen} />
+      <Container style={{ marginTop: '7em' }}>
+        <ActivityDashboard 
+          activities={activities} 
+          selectedActivity={selectedActivity}
+          selectActivity={handleSelectActivity}  
+          cancelSelectActivity={handleCancelSelectActivity}
+          editMode={editMode}
+          openForm={handleFormOpen}
+          closeForm={handleFormClose}
+          createOrEdit={handleCreateOrEditActivity}
+          deleteActivity={handleDeleteActivity}
+        />
+      </Container>
+
+    </>
+  );
+}
+
+export default App;
